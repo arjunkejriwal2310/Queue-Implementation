68a98eb315fda9d0e4727f4f1dd49e89d62657f8
Arjun Kejriwal <73984306+arjunkejriwal2310@users.noreply.github.com>
2021-11-22 19:04:42 -0500

    Add files via upload

---
+++ UseQueue.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+public class UseQueue
+{
+	public static Scanner keyboard = new Scanner(System.in);
+	public static void main(String[] args)
+	{
+		CS112QueueInterface myQ = new CS112Queue();
+		
+		System.out.println("How many guests are there?");
+		int guests = keyboard.nextInt();
+
+		System.out.println("What is the value of N?");
+		int N = keyboard.nextInt();
+
+		for (int i = 1; i <= guests; i++)
+		{
+			myQ.enqueue(i);
+		}
+		myQ.writeQueue();
+
+		int pasta = myQ.dequeue();
+		System.out.println("");
+		System.out.println("The person in seat " + pasta + " just got their hair pastaed");
+		myQ.writeQueue();
+
+		for (int i = 0; i < guests - 2; i++)
+		{
+			for (int j = 0; j < N - 1; j++)
+			{
+				int safe = myQ.dequeue();
+				myQ.enqueue(safe);
+			}
+
+			pasta = myQ.dequeue();
+			System.out.println("");
+			System.out.println("The person in seat " + pasta + " just got their hair pastaed");
+			myQ.writeQueue();
+		}
+
+		int winner = myQ.peek();
+		System.out.println("");
+		System.out.println("The person in seat " + winner + " gets the gold coin");
+	}
+}
